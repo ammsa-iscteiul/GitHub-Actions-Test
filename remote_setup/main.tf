34fc229451c187250bdaec710a8448e5ebf32bbe
Alexandre Silva <alexandre.silva@innowave.tech>
2024-04-22 16:55:40 +0100

    update main.tf

--- remote_setup/main.tf
+++ remote_setup/main.tf
@@ -4,7 +4,6 @@
 
 locals {
   resource_group_name    = "${var.naming_prefix}-${random_integer.sa_num.result}"
-  storage_account_name   = "${lower(var.naming_prefix)}${random_integer.sa_num.result}"
   service_principal_name = "${var.naming_prefix}-${random_integer.sa_num.result}"
 }
 
@@ -38,40 +37,14 @@ resource "azurerm_role_assignment" "gh_actions" {
   principal_id         = azuread_service_principal.gh_actions.id
 }
 
-# Azure Storage Account
 
-resource "random_integer" "sa_num" {
-  min = 10000
-  max = 99999
-}
-
-resource "azurerm_resource_group" "setup" {
-  name     = local.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = local.storage_account_name
-  resource_group_name      = azurerm_resource_group.setup.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-}
-
-resource "azurerm_storage_container" "ct" {
-  name                 = "terraform-state"
-  storage_account_name = azurerm_storage_account.sa.name
-
-}
 
 ## GitHub secrets
 
 resource "github_actions_secret" "actions_secret" {
   for_each = {
-    STORAGE_ACCOUNT     = azurerm_storage_account.sa.name
+    
     RESOURCE_GROUP      = azurerm_storage_account.sa.resource_group_name
-    CONTAINER_NAME      = azurerm_storage_container.ct.name
     ARM_CLIENT_ID       = azuread_service_principal.gh_actions.application_id
     ARM_CLIENT_SECRET   = azuread_service_principal_password.gh_actions.value
     ARM_SUBSCRIPTION_ID = data.azurerm_subscription.current.subscription_id
