24492e1d67249012b37b684ecde3f500f74667c7
Alexandre Silva <alexmmsilva@hotmail.com>
2024-04-24 10:15:11 +0100

    Update main.tf

--- remote_setup/main.tf
+++ remote_setup/main.tf
@@ -50,13 +50,13 @@ resource "azurerm_resource_group" "setup" {
   location = var.location
 }
 
-#resource "azurerm_storage_account" "sa" {
-#  name                     = local.storage_account_name
-#  resource_group_name      = azurerm_resource_group.setup.name
-#  location                 = var.location
-#  account_tier             = "Standard"
-#  account_replication_type = "LRS"
-#}
+resource "azurerm_storage_account" "sa" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.setup.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
 resource "azurerm_storage_container" "ct" {
   name                 = "terraform-state"
